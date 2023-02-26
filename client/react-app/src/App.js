92464af416d2bcb914e93bc6123c4f80553a5333
Kevin Sangston <Layhooo2@users.noreply.github.com>
2023-02-26 12:38:35 -0500

    standings

--- client/react-app/src/App.js
+++ client/react-app/src/App.js
@@ -1,18 +1,25 @@
 import './App.css';
+
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom/client';
 import StandingsTable from './Components/Components.js';
 import NavBar from './Components/Navbar.js';
+=======
 
+import React from "react";
+//<Standings />
 function App() {
   return (
     <div className="App">
       <div className="content">
         <h1>Main App Component</h1>
+          <Standings />
       </div>
       <StandingsTable />
     </div>
   );
 }
 
+
 export default App;
+

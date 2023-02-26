9abb4a4be3ba433e8d6de952df59a6fd0d05846e
Kevin Sangston <Layhooo2@users.noreply.github.com>
2023-02-26 15:45:05 -0500

    Added basic standings table

--- client/react-app/src/App.js
+++ client/react-app/src/App.js
@@ -2,20 +2,17 @@ import './App.css';
 
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom/client';
-import StandingsTable from './Components/Components.js';
-import NavBar from './Components/Navbar.js';
-=======
+import StandingsTable from './Components/StandingsTable';
+//import NavBar from './Components/Navbar.js';
 
-import React from "react";
 //<Standings />
 function App() {
   return (
     <div className="App">
       <div className="content">
         <h1>Main App Component</h1>
-          <Standings />
       </div>
-      <StandingsTable />
+        <StandingsTable />
     </div>
   );
 }

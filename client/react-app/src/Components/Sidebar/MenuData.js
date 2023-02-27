548a94e1c187f6b36cef51f4b35f67b5637db25d
Dylan Drumm <dyl.drumm@gmail.com>
2023-02-27 18:06:11 -0500

    Updated Sidebar

--- client/react-app/src/Components/Sidebar/MenuData.js
+++ client/react-app/src/Components/Sidebar/MenuData.js
@@ -1,18 +1,18 @@
 export const MenuData = [
     {
-        title: "Home",
+        title: "Home \n",
         url: "#",
         cName: "nav-links",
         icon: "fa-solid fa-house-user ",
     },
     {
-        title: "Team Dashboards",
+        title: "Team Dashboards \n",
         url: "#",
         cName: "nav-links",
         icon: "fa-solid fa-basketball-ball ",
     },
     {
-        title: "Player Dashboards",
+        title: "Player Dashboards \n",
         url: "#",
         cName: "nav-links",
         icon: "fa-solid fa-user-alt ",
--- client/react-app/src/Components/Sidebar/Sidebar.js
+++ client/react-app/src/Components/Sidebar/Sidebar.js
@@ -10,13 +10,13 @@ class Sidebar extends Component {
     render() {
         return (
             <nav className="Sidebar">
-                <ul>
+                <ul className = "SidebarItems">
                     {MenuData.map((item, index)=>{
                         return(
                             <li key={index}>
                                 <a href = {item.url} className = {item.cName}>
-                                    <i classname={item.icon}></i>
                                     {item.title}
+                                    <i className={item.icon}></i>
                                 </a>
                             </li>
                         )

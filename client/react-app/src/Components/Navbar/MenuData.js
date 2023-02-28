73c4bb76ac8555192cfe8c9157d56331fdb68116
Dylan Drumm <dyl.drumm@gmail.com>
2023-02-27 23:27:49 -0500

    Sidebar, padding changes
    Made sidebar transparent when extended.  Changed padding on Navbar to center it within the extended sidebar.

--- client/react-app/src/Components/Navbar/MenuData.js
+++ client/react-app/src/Components/Navbar/MenuData.js
@@ -1,14 +1,20 @@
 export const MenuData = [
     {
-        title: "Home",
+        title: " Home",
         url: "#",
         cName:"nav-links",
         icon:"fa-solid fa-house-user "
     },
     {
-        title: "About Us",
+        title: " About Us",
         url: "#",
         cName:"nav-links",
         icon:"fa-solid fa-circle-info "
-    }
+    },
+    {
+        title: " Profile",
+        url: "#",
+        cName:"nav-links",
+        icon:"fa-solid fa-user-alt  "
+    },
 ]
